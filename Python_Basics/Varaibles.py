1289d984dba92a762bb9a5f7213013d43161ba33
NaveenNayak69 <banothnaveen1436@gmail.com>
2026-05-19 22:15:50 +0530

    Add basic Python variables and data types example

---
+++ Python_Basics/Varaibles.py
@@ -0,0 +1,28 @@
+print("Hello, World!")
+
+#PYTHON VARIABLES
+
+name = "Naveen"
+age = 23
+height = 5.4
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Variable reassignment
+name = "Naveen Kumar"
+age = 24
+height = 5.6
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+# Variable types
+print("Type of name:", type(name))  
+print("Type of age:", type(age))
+print("Type of height:", type(height))
+
+#data types
+is_student = True 
+print("Is student:", is_student)
+print("Type of is_student:", type(is_student))
