303f07bb0ac9b00c3c1fff2ebd5ed9743e7da1ab
NaveenNayak69 <banothnaveen1436@gmail.com>
2026-05-19 22:21:26 +0530

    Add arithmetic operations in Arithematic.py

---
+++ Python_Basics/Arithematic.py
@@ -0,0 +1,30 @@
+#arithmetic Operations
+a = 20
+b = 5
+# Addition
+addition = a + b    
+print("Addition:", addition)
+
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+# Division
+division = a / b
+print("Division:", division)
+
+# Modulus
+modulus = a % b 
+print("Modulus:", modulus)
+
+# Floor Division
+floor_division = a // b
+print("Floor Division:", floor_division)
+
+# Exponentiation
+exponentiation = a ** b
+print("Exponentiation:", exponentiation)
