bb2fa1aae87f216fc9cc0d15f8705e032a31e325
Banoth Naveen <banothnaveen1436@gmail.com>
2026-05-20 17:01:11 +0530

    Add assignment operators examples in assignment_operator.py

---
+++ Python_Basics/assignment_operator.py
@@ -0,0 +1,30 @@
+#assignment Operators
+a = 10
+b = 20
+# Addition assignment
+a += b
+print("Addition assignment:", a)
+
+# Subtraction assignment
+a -= b
+print("Subtraction assignment:", a)
+
+# Multiplication assignment
+a *= b
+print("Multiplication assignment:", a)
+
+# Division assignment
+a /= b
+print("Division assignment:", a)
+
+# Modulus assignment
+a %= b
+print("Modulus assignment:", a)
+
+# Floor Division assignment
+a //= b
+print("Floor Division assignment:", a)
+
+# Exponentiation assignment
+a **= b
+print("Exponentiation assignment:", a)
