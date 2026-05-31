7573fddd580a51c149a943e47e0f84ed881b44e6
Banoth Naveen <banothnaveen1436@gmail.com>
2026-05-31 11:46:13 +0530

    Implement user input for personal details
    Add user input functionality for name, age, and height, along with a practice task to print the length of the first name.

---
+++ Python_Basics/input.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+#Practice 2 : WAP to input user's first name and print its length
+
+#WAP to input user's first name and print its length
+first_name = input("Enter your first name: ")   
+print("Length of your first name is:", len(first_name))
